564fd5900b1b573af6495f6e9f670fe81372c6c1
LeatherFire <71224249+LeatherFire@users.noreply.github.com>
2024-06-03 13:28:26 +0300

    Update user.js

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,9 +1,11 @@
 import { getSession } from "next-auth/react";
 import User from "@/models/User";
 import dbConnect from "@/util/dbConnect";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./auth/[...nextauth]";
 
 export default async function handler(req, res) {
-  const session = await getSession({ req });
+  const session = await getServerSession(req, res, authOptions);
 
   if (!session) {
     return res.status(401).json({ message: "Unauthorized" });
