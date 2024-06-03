7a263188887d2f127a36ca6ec31bc1a1e9342607
Yiğitcan Uçar <71224249+LeatherFire@users.noreply.github.com>
2024-06-03 10:36:09 +0300

    Update user.js

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,15 +1,16 @@
-import { getSession } from "next-auth/react";
+import { getServerSession } from "next-auth/next";
 import User from "@/models/User";
 import dbConnect from "@/util/dbConnect";
+import { authOptions } from "./auth/[...nextauth]";
 
 export default async function handler(req, res) {
-  const session = await getSession({ req });
+  await dbConnect();
+
+  const session = await getServerSession(req, res, authOptions);
 
   if (!session) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-
-  await dbConnect();
 
   const user = await User.findOne({ email: session.user.email }).select("-password");
 
